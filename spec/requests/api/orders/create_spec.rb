0f048175cc42bb8254a95dfe8738c0a7a73e6874
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-07 10:47:41 +0100

    add empty model/class/resource

--- spec/requests/api/orders/create_spec.rb
+++ spec/requests/api/orders/create_spec.rb
@@ -1,6 +1,6 @@
 #1. we need a Product so we actually have something to buy
 #2. We need a user so we know who ordered a product
-#3. we need to ba able to create an order and connect/associate with user
+#3. we need to ba able to create an Order and connect/associate with user
 #4. we need to associate an order with a product using joint table that we will call OrderItem
 
 RSpec.describe "POST /api/orders" do
