9d629ef61f5e7760bce7c0c64e7d5abeb3a0385c
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-12 19:41:17 +0100

    add spec for serialized and pass it

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :items, class_name: 'OrderItem'
   has_many :products, through: :items
+
+  def serialized
+    serialization = as_json
+    serialization.merge!(products: products.as_json)
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -22,4 +22,20 @@ RSpec.describe Order, type: :model do
       expect(create(:order)).to be_valid
     end
   end
+
+  describe 'Instance methods' do
+    describe '#serialized' do
+      let(:product1) { create(:product, name: 'Pancakes') }
+      let(:product2) { create(:product, name: 'Soup') }
+      let!(:order) { create(:order) }
+      let!(:order_item1) { create(:order_item, order: order, product: product1) }
+      let!(:order_item2) { create(:order_item, order: order, product: product2) }
+
+      it { is_expected.to respond_to :serialized }
+
+      it 'is expected to return json object including items' do
+        expect(order.serialized[:products].size).to eq 2
+      end
+    end
+  end
 end
